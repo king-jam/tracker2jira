ee1f69f6d2b06dd847ac9fc0b80d6fa227a3fb95
Ethan Kaley <ethan.kaley@emc.com>
2017-10-20 16:37:52 -0400

    Refactor backend storage
    Signed-off-by: James King <james.king@emc.com>

--- backend/store.go
+++ backend/store.go
@@ -38,21 +38,3 @@ func ConfigureDB() error {
 	}
 	return nil
 }
-
-// GetVersion ...
-func (b *Backend) GetVersion() (string, error) {
-	pair, err := b.store.Get(versionPath)
-	if err != nil {
-		log.Printf("no version")
-	}
-	return string(pair.Value), nil
-}
-
-// PutVersion ...
-func (b *Backend) PutVersion(version string) error {
-	err := b.store.Put(versionPath, []byte(version), nil)
-	if err != nil {
-		log.Printf("failed to set version")
-	}
-	return nil
-}
